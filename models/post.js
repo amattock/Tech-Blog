216c5c6920f3eee1f24713ac0e81bc058edacf4b
amattock <134568545+amattock@users.noreply.github.com>
2023-10-01 15:12:29 -0500

    added models and index's for controllers and api folders

---
+++ models/post.js
@@ -0,0 +1,45 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+// const { post } = require('.');
+
+class post extends Model {}
+
+post.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    postTitle: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    postBody: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    date_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'post',
+  }
+);
+
+module.exports = post;
